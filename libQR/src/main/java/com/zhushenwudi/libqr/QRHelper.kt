278c8e0bc747bb7e6d37132701092cf1336e1662
gr.zhu <55681140@163.com>
2022-04-02 12:04:56 +0800

    重构代码

--- libQR/src/main/java/com/zhushenwudi/libqr/QRHelper.kt
+++ libQR/src/main/java/com/zhushenwudi/libqr/QRHelper.kt
@@ -3,32 +3,25 @@ package com.zhushenwudi.libqr
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.USB_SERVICE
 import android.content.Intent
 import android.content.IntentFilter
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbDeviceConnection
 import android.hardware.usb.UsbManager
-import android.os.Binder
-import android.os.IBinder
 import android.util.Log
-import androidx.lifecycle.LifecycleService
-import androidx.lifecycle.MutableLiveData
 import com.felhr.usbserial.UsbSerialDevice
-import com.felhr.usbserial.UsbSerialInterface.*
-import com.kunminx.architecture.ui.callback.UnPeekLiveData
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.felhr.usbserial.UsbSerialInterface
+import kotlinx.coroutines.*
 import java.io.UnsupportedEncodingException
 
-class QRService: LifecycleService() {
+class QRHelper(private val context: Context, private val callback: (code: String) -> Unit) {
     private lateinit var usbManager: UsbManager
     private var device: UsbDevice? = null
     private var connection: UsbDeviceConnection? = null
     private var serialPort: UsbSerialDevice? = null
     private var scope: CoroutineScope? = null
-    private var mHandler: UnPeekLiveData<String>? = null
-    private var serialPortConnected = false
+    private var isConnected = false
 
     private val usbReceiver = object : BroadcastReceiver() {
         override fun onReceive(arg0: Context, mIntent: Intent) {
@@ -37,86 +30,58 @@ class QRService: LifecycleService() {
                 granted?.let {
                     if (granted) {
                         val intent = Intent(ACTION_USB_PERMISSION_GRANTED)
-                        arg0.sendBroadcast(intent)
+                        context.sendBroadcast(intent)
                         connection = usbManager.openDevice(device)
                         scope = CoroutineScope(Dispatchers.IO)
                         scope?.launch {
                             serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection)
                             serialPort?.run {
                                 if (open()) {
-                                    serialPortConnected = true
+                                    isConnected = true
                                     setBaudRate(BAUD_RATE)
-                                    setDataBits(DATA_BITS_8)
-                                    setStopBits(STOP_BITS_1)
-                                    setParity(PARITY_NONE)
-                                    setFlowControl(FLOW_CONTROL_OFF)
+                                    setDataBits(UsbSerialInterface.DATA_BITS_8)
+                                    setStopBits(UsbSerialInterface.STOP_BITS_1)
+                                    setParity(UsbSerialInterface.PARITY_NONE)
+                                    setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF)
                                     read {
                                         try {
-                                            mHandler?.postValue(String(it, Charsets.UTF_8))
+                                            val str = String(it, Charsets.UTF_8).trim()
+                                            if (str.isNotEmpty()) {
+                                                callback(str)
+                                            }
                                         } catch (e: UnsupportedEncodingException) {
                                             e.printStackTrace()
                                         }
                                     }
                                 }
                             } ?: run {
-                                sendBroadcast(Intent(ACTION_USB_NOT_SUPPORTED))
+                                context.sendBroadcast(Intent(ACTION_USB_NOT_SUPPORTED))
                             }
                         }
                     } else {
                         val intent = Intent(ACTION_USB_PERMISSION_NOT_GRANTED)
-                        arg0.sendBroadcast(intent)
+                        context.sendBroadcast(intent)
                     }
                 }
             } else if (mIntent.action == ACTION_USB_ATTACHED) {
-                if (!serialPortConnected) {
+                if (!isConnected) {
                     findSerialPortDevice()
                 }
             } else if (mIntent.action == ACTION_USB_DETACHED) {
                 val intent = Intent(ACTION_USB_DISCONNECTED)
-                arg0.sendBroadcast(intent)
-                if (serialPortConnected) {
+                context.sendBroadcast(intent)
+                if (isConnected) {
                     serialPort?.close()
                 }
-                serialPortConnected = false
+                isConnected = false
             }
         }
     }
 
-    init {
-        serialPortConnected = false
-        SERVICE_CONNECTED = true
-    }
-
-    /**
-     * 向二维码模块下发字节数组指令
-     */
-    fun write(data: ByteArray?) {
-        serialPort?.write(data)
-    }
-
-    /**
-     * 扫描一次
-     */
-    fun readOnce() {
-        serialPort?.write(HexUtil.decodeHex(READ_ONCE))
-    }
-
-    /**
-     * 连续扫描
-     */
-    fun readContinuous() {
-        serialPort?.write(HexUtil.decodeHex(READ_CONTINUOUS))
-    }
-
-    /**
-     * 停止扫描
-     */
-    fun stopRead() {
-        serialPort?.write(HexUtil.decodeHex(STOP_READ))
-    }
-
-    fun setHandler(handler: UnPeekLiveData<String>) {
-        mHandler = handler
+    private fun init() {
+        setFilter()
+        usbManager = context.getSystemService(USB_SERVICE) as UsbManager
+        findSerialPortDevice()
     }
 
     /**
@@ -142,11 +107,11 @@ class QRService: LifecycleService() {
             }
             if (keep) {
                 val intent = Intent(ACTION_NO_USB)
-                sendBroadcast(intent)
+                context.sendBroadcast(intent)
             }
         } else {
             val intent = Intent(ACTION_NO_USB)
-            sendBroadcast(intent)
+            context.sendBroadcast(intent)
         }
     }
 
@@ -158,12 +123,12 @@ class QRService: LifecycleService() {
         filter.addAction(ACTION_USB_PERMISSION)
         filter.addAction(ACTION_USB_DETACHED)
         filter.addAction(ACTION_USB_ATTACHED)
-        registerReceiver(usbReceiver, filter)
+        context.registerReceiver(usbReceiver, filter)
     }
 
     private fun requestUserPermission() {
         val mPendingIntent = PendingIntent.getBroadcast(
-            this,
+            context,
             0,
             Intent(ACTION_USB_PERMISSION),
             0
@@ -171,24 +136,50 @@ class QRService: LifecycleService() {
         usbManager.requestPermission(device, mPendingIntent)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(usbReceiver)
-        SERVICE_CONNECTED = false
+    /**
+     * 向二维码模块下发字节数组指令
+     */
+    suspend fun write(data: ByteArray?) {
+        checkConnected()
+        serialPort?.write(data)
     }
 
-    override fun onBind(intent: Intent): IBinder {
-        super.onBind(intent)
-        setFilter()
-        usbManager = getSystemService(USB_SERVICE) as UsbManager
-        findSerialPortDevice()
-        return QRBinder(this)
+    /**
+     * 扫描一次
+     */
+    suspend fun readOnce() {
+        checkConnected()
+        serialPort?.write(HexUtil.decodeHex(READ_ONCE))
     }
 
-    internal class QRBinder(private val service: QRService) : Binder() {
-        fun getService(): QRService {
-            return service
+    /**
+     * 连续扫描
+     */
+    suspend fun readContinuous() {
+        checkConnected()
+        serialPort?.write(HexUtil.decodeHex(READ_CONTINUOUS))
+    }
+
+    /**
+     * 停止扫描
+     */
+    fun stopRead() {
+        serialPort?.write(HexUtil.decodeHex(STOP_READ))
+    }
+
+    private suspend fun checkConnected() {
+        if (!isConnected) {
+            init()
+            delay(300)
         }
+    }
+
+    fun release() {
+        context.unregisterReceiver(usbReceiver)
+        if (scope?.isActive == true) {
+            scope?.cancel()
+        }
+        scope = null
     }
 
     companion object {
@@ -197,13 +188,13 @@ class QRService: LifecycleService() {
         const val ACTION_USB_NOT_SUPPORTED = "com.felhr.usbservice.USB_NOT_SUPPORTED"
         const val ACTION_NO_USB = "com.felhr.usbservice.NO_USB"
         const val ACTION_USB_PERMISSION_GRANTED = "com.felhr.usbservice.USB_PERMISSION_GRANTED"
-        const val ACTION_USB_PERMISSION_NOT_GRANTED = "com.felhr.usbservice.USB_PERMISSION_NOT_GRANTED"
+        const val ACTION_USB_PERMISSION_NOT_GRANTED =
+            "com.felhr.usbservice.USB_PERMISSION_NOT_GRANTED"
         const val ACTION_USB_DISCONNECTED = "com.felhr.usbservice.USB_DISCONNECTED"
         private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
         const val READ_ONCE = "020700535730303030309A0103"
         const val READ_CONTINUOUS = "020700535730303030319B0103"
         const val STOP_READ = "02070053574646464646080203"
         private const val BAUD_RATE = 9600
-        var SERVICE_CONNECTED = false
     }
 }
